135d0695f3b76a34cc2ab68f1e0d0041b4684575
Bart Rylant <bart.rylant@gmail.com>
2012-10-23 01:54:32 +0200

    Dutch translation
    Added a dutch translation.

---
+++ language/dutch/lang.calendar.php
@@ -0,0 +1,934 @@
+<?php
+
+ /**
+ * Solspace - Calendar
+ *
+ * @package		Solspace:Calendar
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2010-2012, Solspace, Inc.
+ * @link		http://www.solspace.com/docs/addon/c/Calendar/
+ * @version		1.7.1
+ * @filesource 	./system/expressionengine/third_party/calendar/language/english/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+'calendar_module_name' =>
+'Calendar',
+
+'calendar_module_description' =>
+'Een heel complete kalendermodule voor ExpressionEngine',
+
+'calendar_module_version' =>
+'Calendar',
+
+//----------------------------------------
+//  Installation
+//----------------------------------------
+
+'calendars_field_group_already_exists' =>
+'De veldengroep "Calendar: Calendars" bestaat al.',
+
+'events_field_group_already_exists' =>
+'De veldengroep "Calendar: Events" bestaat al.',
+
+'calendars_weblog_already_exists' =>
+'Het kanaal "Calendar: Calendars" bestaat al.',
+
+'events_weblog_already_exists' =>
+'Het kanaal "Calendar: Events" bestaat al.',
+
+'cannot_install' =>
+'Calendar kan niet geïnstalleerd worden zolang deze fouten niet zijn opgelost:',
+
+//----------------------------------------
+//  Upgradeification
+//----------------------------------------
+
+'update_calendar' =>
+'Calendar updaten',
+
+"update_successful"	=>
+"De module is geüpdatet.",
+
+'calendar_update_message' =>
+'Er is een nieuwe versie van Calendar. Wil je deze versie updaten?',
+
+'update_failure' =>
+'De Calendar module update is mislukt.',
+
+'update_successful' =>
+'De Calendar module update is geslaagd.',
+
+//----------------------------------------
+//  Main Menu
+//----------------------------------------
+
+'calendars' =>
+'Kalenders',
+
+'events' =>
+'Evenementen',
+
+'occurrences' =>
+'Herhalingen',
+
+'reminders' =>
+'Herinneringen',
+
+'permissions' =>
+'Toelatingen',
+
+'preferences' =>
+'Voorkeuren',
+
+'documentation' =>
+'Online Documentation',
+
+'online_documentation' =>
+'On line documentatie',
+
+//----------------------------------------
+//  Publish/Edit
+//----------------------------------------
+
+'filter' =>
+'Filter',
+
+'remove_edited_occurrences' =>
+'Verwijder gewijzigde herhalingen',
+
+'remove_edited_occurrences_desc' =>
+'Vink deze optie uit als je enkel details wijzigt voor dit evenement. Het wordt aangeraden de optie AANGEVINKT te laten als je TIJDEN, BEGINDATA of EINDDATA van een evenement wil veranderen. Het uitvinken van de optie zorgt in dat geval voor losgekoppelde evenementen.',
+
+'create_calendar_first' =>
+'Je moet een kalender maken voor je een evenement kan maken.',
+
+'select_a_calendar' =>
+'Selecteer een kalender',
+
+'new_date' =>
+'Nieuwe datum',
+
+'add_rule' =>
+'Regel toevoegen',
+
+'editing_occurrence' =>
+'Bezig met wijzigen van de herhaling op <strong>%date%</strong> van <strong>%title%</strong>',
+
+'type' =>
+'Type',
+
+'include' =>
+'Inclusief',
+
+'exclude' =>
+'Exclusief',
+
+'repeat' =>
+'Herhaal',
+
+'none' =>
+'Niet',
+
+'daily' =>
+'Dagelijks',
+
+'weekly' =>
+'Wekelijks',
+
+'monthly' =>
+'Maandelijks',
+
+'yearly' =>
+'Jaarlijks',
+
+'select_dates' =>
+'Selecteer data',
+
+'all_day_event' =>
+'Hele dag',
+
+'from' =>
+'Van',
+
+'to' =>
+'Tot',
+
+'every' =>
+'Elke',
+
+'day_s' =>
+'dag(en)',
+
+'week_s_on' =>
+'we(e)k(en) op',
+
+'at' =>
+'om',
+
+'month_s_by_day_of' =>
+'maand(en) per dag van',
+
+'1st' =>
+'1e',
+
+'2nd' =>
+'2e',
+
+'3rd' =>
+'3e',
+
+'4th' =>
+'4e',
+
+'5th' =>
+'5e',
+
+'only_on' =>
+'Enkel op',
+
+'year_s' =>
+'ja(a)r(en)',
+
+'end' =>
+'Einde',
+
+'never' =>
+'Nooit',
+
+'by_date' =>
+'op datum',
+
+'after' =>
+'Na',
+
+'time_s' =>
+'keer',
+
+//----------------------------------------
+//  CP Calendars
+//----------------------------------------
+
+'calendar_id' =>
+'Kalender ID',
+
+'calendar_name' =>
+'Kalendernaam',
+
+'status' =>
+'Status',
+
+'total_events' =>
+'Totaal aantal evenenten',
+
+//----------------------------------------
+//  CP Events
+//----------------------------------------
+
+'event_id' =>
+'Evenent ID',
+
+'event_name' =>
+'Evenementnaam',
+
+'recurs' =>
+'Herhaalt',
+
+'first_date' =>
+'Eerste datum',
+
+'last_date' =>
+'Laatste datum',
+
+'filter_events' =>
+"Filter evenementen",
+
+'filter_by_calendar' =>
+'Filter op kalender',
+
+'filter_by_status' =>
+'Filter op status',
+
+'filter_by_recurs' =>
+'Filter op herhalingen',
+
+'order_by' =>
+'Sorteer op',
+
+'date_is' =>
+'Datum is',
+
+'event_id' =>
+'Evenement ID',
+
+'event_title' =>
+'Evenementtitel',
+
+'calendar_name' =>
+'Kalendernaam',
+
+'status' =>
+'Status',
+
+'recurs' =>
+'Herhaalt',
+
+'first_date_is' =>
+'Eerste datum is',
+
+'last_date' =>
+'Laatste datum',
+
+'ascending' =>
+'Oplopend',
+
+'descending' =>
+'Aflopend',
+
+'or_later' =>
+'of later',
+
+'or_earlier' =>
+'of vroeger',
+
+'this_date' =>
+'deze datum',
+
+'time' =>
+'Tijd',
+
+'all_day' =>
+'Hele dag',
+
+//----------------------------------------
+//  CP Events Delete
+//----------------------------------------
+
+'delete' =>
+'Verwijderen',
+
+'delete_events' =>
+'Verwijder evenementen',
+
+'delete_events_title' =>
+'Evenement(en) verwijderen?',
+
+'delete_events_question' =>
+'Heel zeker {COUNT} evenement(en) verwijderen?',
+
+'events_deleted' =>
+'Evenement(en) zijn verwijderd',
+
+//----------------------------------------
+//  CP Occurrences
+//----------------------------------------
+
+'occurrence_id' =>
+'Herhaling ID',
+
+'event_date' =>
+'Evenementdatum',
+
+'limit' =>
+'Limiet',
+
+'page_limit' =>
+'Paginalimiet',
+
+'occurrences_limit' =>
+'Herhalingslimiet',
+
+// -------------------------------------
+//	Permissions
+// -------------------------------------
+
+'calendar_permissions_desc' =>
+"Toelatingen kunnen ingesteld worden bij het maken van een kalender, of hier aangepast worden. Super Admins en de geselecteerde groepen in 'Verleen volledige toegang voor groepen' hebben toegang tot alle kalenders. Geselecteerde groepen in 'Weiger alle toegang voor groepen' hebben tot geen enkel kalender toegang.",
+
+'allowed_groups' =>
+"Toegelaten groepen",
+
+'allow_full_access' =>
+'Verleen volledige toegang voor groepen',
+
+'permissions_enabled' =>
+'Toelatingen geactiveerd',
+
+'save_permissions' =>
+'Toelatingen opslaan',
+
+'all_groups' =>
+'Alle groepen',
+
+
+'allow_all' =>
+'Allemaal toelaten',
+
+'deny_all_access' =>
+'Weiger alle toegang voor groepen',
+
+'deny_takes_precedence' =>
+"heeft voorrang op Allemaal toelaten",
+
+'permissions_saved' =>
+"Toelatingen opgeslagen",
+
+'group_permissions' =>
+'Toelatingen per groep',
+
+'permissions_instructions' =>
+"Selecteer de groepen die de kalender en zijn evenementen mogen wijzigen. (Super Admins hebben altijd toegang tot alle kalenders). Als een groep niet getoond wordt, zit die ofwel in de Alles toestaan- of Alles weigerenlijst onder de tab Toelatingen in het Calendar controlepaneel.",
+
+'disallowed_behavior_for_edit_page' =>
+"Niet toegestaan bij wijzigen",
+
+'none' =>
+"Geen",
+
+'search_filter' =>
+"Zoekfilter",
+
+'disable_link' =>
+"Link verwijderen",
+
+'permission_dialog_title' =>
+"Fout met toelatingen",
+
+'ok' => "OK",
+
+//----------------------------------------
+//  CP Preferences
+//----------------------------------------
+
+'preference' =>
+'Voorkeur',
+
+'setting' =>
+'Instelling',
+
+'description' =>
+'Beschrijving',
+
+'first_day_of_week' =>
+'Eerste dag van de week',
+
+'first_day_of_week_description' =>
+'Maandag of zondag zijn de meest voordehandliggende opties.',
+
+'clock_type' =>
+'Klokweergave',
+
+'clock_type_description' =>
+'Gebruik 12-uur of 24-uur weergave in controlepaneel?',
+
+'12_hour' =>
+'12-uur',
+
+'24_hour' =>
+'24-uur',
+
+'default_timezone' =>
+'Standaard tijdzone',
+
+'default_timezone_description' =>
+'Standaard tijdzone voor nieuwe kalenders.',
+
+'preferences_updated' =>
+'Voorkeuren geüpdateted.',
+
+'default_date_format' =>
+'Datumnotatie voor datumkiezer',
+
+'default_date_format_description' =>
+'Datumnotatie voor datumkiezer.',
+
+'default_time_format' =>
+'Standaard tijdnotatie',
+
+'default_time_format_description' =>
+'Standaard tijdnotatie voor nieuwe kalenders.',
+
+'calendar_weblog' =>
+'Kalender weblog(s)',
+
+'calendar_weblog_description' =>
+'Weblog(s) om te zetten naar Calendar weblog(s)',
+
+'event_weblog' =>
+'Evenementen weblog(s)',
+
+'event_weblog_description' =>
+'Weblog(s) om te zetten naar weblog(s)',
+
+//----------------------------------------
+//  Buttons
+//----------------------------------------
+
+'save' =>
+'Opslaan',
+
+'delete_selected_items' =>
+'Geselecteerde items verwijderen',
+
+//----------------------------------------
+//  Errors
+//----------------------------------------
+
+'no_results' =>
+'Geen resultaten.',
+
+'no_title' =>
+'Geen titel',
+
+'invalid_request' =>
+"Ongeldige vraag",
+
+'calendar_module_disabled' =>
+"De Calendar module is momenteel uitgeschakeld. Controleer of de module is geïnstalleerd en up-to-date is in module controlepaneel in het ExpressionEngine controlepaneel.",
+
+'disable_module_to_disable_extension' =>
+"Om deze extensie uit te schakelen, moet je de bijbehorende <a href='%url%'>module</a> uitschakelen.",
+
+'enable_module_to_enable_extension' =>
+"Om deze extensie in te schakelen, moet je de bijbehorende <a href='%url%'>module</a> inschakelen.",
+
+'cp_jquery_requred' =>
+"De 'jQuery for the Control Panel' extensie moet <a href='%extensions_url%'>ingeschakeld</a> zijn om deze module te kunnn gebruiken.",
+
+'invalid_weblog_id' =>
+'Ongeldige weblog ID',
+
+'invalid_entry_id' =>
+'Ongeldige entry ID',
+
+'invalid_site_id' =>
+'Ongeldige site ID',
+
+'invalid_calendar_id' =>
+'Ongeldige kalender ID',
+
+'invalid_ymd' =>
+'Ongeldige datum',
+
+'invalid_start_date' =>
+'Ongeldige begindatum',
+
+'invalid_end_date' =>
+'Ongeldige einddatum',
+
+'invalid_year' =>
+'Ongeldig jaar',
+
+'invalid_month' =>
+'Ongeldige maand',
+
+'invalid_day' =>
+'Ongeldige dag',
+
+'invalid_date' =>
+'Ongeldige datum',
+
+'invalid_time' =>
+'Ongeldige tijd',
+
+'invalid_start_time' =>
+'Ongeldige begintijd',
+
+'invalid_end_time' =>
+'Ongeldige eindtijd',
+
+'invalid_hour' =>
+'Ongeldig uur',
+
+'invalid_minute' =>
+'Ongeldige minuut',
+
+'invalid_repeat_dates' =>
+'Ongeldige herhaaldata',
+
+'invalid_calendar_permissions' =>
+'Je hebt geen toelating deze kalender te wijzigen of evenementen toe te voegen aan deze kalender.',
+
+'no_permissions_for_any_calendars' =>
+"Je hebt geen toelating evenementen te wijzigen of toe te wijzen aan kalenders.",
+
+'invalid_permissions_json_request' =>
+"Ongeldige JSON request. Vereist group_id and EE 2.x+.",
+
+//----------------------------------------
+//  Days
+//----------------------------------------
+
+'day_1_full' =>
+'maandag',
+
+'day_2_full' =>
+'dinsdag',
+
+'day_3_full' =>
+'woensdag',
+
+'day_4_full' =>
+'donderdag',
+
+'day_5_full' =>
+'vrijdag',
+
+'day_6_full' =>
+'zaterdag',
+
+'day_0_full' =>
+'zondag',
+
+//----------------------------------------
+//  Days - 2 Letters
+//----------------------------------------
+
+'day_1_2' =>
+'ma',
+
+'day_2_2' =>
+'di',
+
+'day_3_2' =>
+'wo',
+
+'day_4_2' =>
+'do',
+
+'day_5_2' =>
+'vr',
+
+'day_6_2' =>
+'za',
+
+'day_0_2' =>
+'zo',
+
+//----------------------------------------
+//  Days - 3 Letters
+//----------------------------------------
+
+'day_1_3' =>
+'Mon',
+
+'day_2_3' =>
+'Tue',
+
+'day_3_3' =>
+'Wed',
+
+'day_4_3' =>
+'Thu',
+
+'day_5_3' =>
+'Fri',
+
+'day_6_3' =>
+'Sat',
+
+'day_0_3' =>
+'Sun',
+
+//----------------------------------------
+//  Days - Short
+//----------------------------------------
+
+'day_1_short' =>
+'maa',
+
+'day_2_short' =>
+'din',
+
+'day_3_short' =>
+'woe',
+
+'day_4_short' =>
+'don',
+
+'day_5_short' =>
+'vrij',
+
+'day_6_short' =>
+'zat',
+
+'day_0_short' =>
+'zon',
+
+//----------------------------------------
+//  Days - 1 letter
+//----------------------------------------
+
+'day_1_1' =>
+'m',
+
+'day_2_1' =>
+'d',
+
+'day_3_1' =>
+'w',
+
+'day_4_1' =>
+'d',
+
+'day_5_1' =>
+'v',
+
+'day_6_1' =>
+'z',
+
+'day_0_1' =>
+'z',
+
+//----------------------------------------
+//  Ordinal suffixes
+//----------------------------------------
+
+'suffix_0' =>
+'e',
+
+'suffix_1' =>
+'e',
+
+'suffix_2' =>
+'e',
+
+'suffix_3' =>
+'e',
+
+'suffix_4' =>
+'e',
+
+'suffix_5' =>
+'e',
+
+'suffix_6' =>
+'e',
+
+'suffix_7' =>
+'e',
+
+'suffix_8' =>
+'e',
+
+'suffix_9' =>
+'e',
+
+'suffix_10' =>
+'e',
+
+'suffix_11' =>
+'e',
+
+'suffix_12' =>
+'e',
+
+'suffix_13' =>
+'e',
+
+'suffix_14' =>
+'e',
+
+'suffix_15' =>
+'e',
+
+'suffix_16' =>
+'e',
+
+'suffix_17' =>
+'e',
+
+'suffix_18' =>
+'e',
+
+'suffix_19' =>
+'e',
+
+//----------------------------------------
+//  Months
+//----------------------------------------
+
+'month_1_full' =>
+'januari',
+
+'month_2_full' =>
+'februari',
+
+'month_3_full' =>
+'maart',
+
+'month_4_full' =>
+'april',
+
+'month_5_full' =>
+'mei',
+
+'month_6_full' =>
+'juni',
+
+'month_7_full' =>
+'juli',
+
+'month_8_full' =>
+'augustus',
+
+'month_9_full' =>
+'september',
+
+'month_10_full' =>
+'oktober',
+
+'month_11_full' =>
+'november',
+
+'month_12_full' =>
+'december',
+
+//----------------------------------------
+//  Months - 3 letters
+//----------------------------------------
+
+'month_1_3' =>
+'jan',
+
+'month_2_3' =>
+'feb',
+
+'month_3_3' =>
+'maa',
+
+'month_4_3' =>
+'apr',
+
+'month_5_3' =>
+'mei',
+
+'month_6_3' =>
+'jun',
+
+'month_7_3' =>
+'jul',
+
+'month_8_3' =>
+'aug',
+
+'month_9_3' =>
+'sep',
+
+'month_10_3' =>
+'okt',
+
+'month_11_3' =>
+'nov',
+
+'month_12_3' =>
+'dec',
+
+//----------------------------------------
+//  am/pm
+//----------------------------------------
+
+'am' =>
+'am',
+
+'pm' =>
+'pm',
+
+'AM' =>
+'AM',
+
+'PM' =>
+'PM',
+
+'am_dot' =>
+'a.m.',
+
+'pm_dot' =>
+'p.m.',
+
+//----------------------------------------
+//  Date parameters
+//----------------------------------------
+
+'today' =>
+'vandaag',
+
+'yesterday' =>
+'gisteren',
+
+'tomorrow' =>
+'morgen',
+
+'day' =>
+'dag',
+
+'week' =>
+'week',
+
+'month' =>
+'maand',
+
+'year' =>
+'jaar',
+
+'ago' =>
+'geleden',
+
+'begin' =>
+'begin',
+
+'last' =>
+'voorbije',
+
+//----------------------------------------
+//  Time parameters
+//----------------------------------------
+
+'now' =>
+'nu',
+
+'noon' =>
+'middag',
+
+'midnight' =>
+'middernacht',
+
+//----------------------------------------
+//  field verbage
+//----------------------------------------
+
+'summary' =>
+'Samenvatting',
+
+'location' =>
+'Locatie',
+
+'dates_and_options' =>
+'Data & opties',
+
+'ics_url_label' =>
+'URL naar iCalendar (.ics) bestand',
+
+'ics_url_desc' =>
+"Voeg een of meerdere URLs naar .ics bestanden - gescheiden door nieuwe regels - toe om ze in deze kalender te importeren. Alle geïmporteerde items worden aangepast naar de ingestelde tijdzone voor deze kalender.",
+
+'ics_url_stub' =>
+"Alle geïmporteerde items worden aangepast naar de ingestelde tijdzone voor deze kalender.",
+
+'time_format_label' =>
+'Tijdnotatie',
+
+'time_format_desc' =>
+'Standaard tijdnotatie gebruiken voor deze kalender.',
+
+'timezone' =>
+'Tijdzone',
+
+/* END */
+''=>''
+);
