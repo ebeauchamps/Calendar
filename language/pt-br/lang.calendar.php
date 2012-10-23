dced97ac9810d6624a5dd0eac9d8fe2b7303ffd0
MarchiMedia <marchimedia@gmail.com>
2012-10-23 20:49:51 -0300

    Added Brazilan Portuguese language
    Added Brazila Portuguese (pt-br) language for Solspace Calendar Module

---
+++ language/pt-br/lang.calendar.php
@@ -0,0 +1,935 @@
+<?php
+
+ /**
+ * Solspace - Calendar
+ *
+ * @package		Solspace:Calendar
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2010-2012, Solspace, Inc.
+ * @link		http://www.solspace.com/docs/addon/c/Calendar/
+ * @version		1.7.1
+ * @translated to Brazilian Portuguese (pt-br) by Marchimedia
+ * @filesource 	./system/expressionengine/third_party/calendar/language/english/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Necessário para a página MÓDULOS
+//----------------------------------------
+
+'calendar_module_name' =>
+'Calendar',
+
+'calendar_module_description' =>
+'Um módulod e calendário completo para o ExpressionEngine',
+
+'calendar_module_version' =>
+'Calendário',
+
+//----------------------------------------
+//  Instalação
+//----------------------------------------
+
+'calendars_field_group_already_exists' =>
+'O grupo de campos "Calendário: Calendários" já existe.',
+
+'events_field_group_already_exists' =>
+'O grupo de campos "Calendário: Eventos" já existe.',
+
+'calendars_weblog_already_exists' =>
+'O canal "Calendário: Calendários" já existe.',
+
+'events_weblog_already_exists' =>
+'The weblog "Calendar: Events" already existe.',
+
+'cannot_install' =>
+'O Módulo Calendário  não pode ser instalado até que estes erros sejam corrigidos:',
+
+//----------------------------------------
+//  Upgradeficação
+//----------------------------------------
+
+'update_calendar' =>
+'Atualizar Calendário',
+
+"update_successful"	=>
+"O módulo foi atualizado com sucesso.",
+
+'calendar_update_message' =>
+'Uma nova versão do Calendário está pronta. Gostaria de atualizar?',
+
+'update_failure' =>
+'A atualização do Módulo Calendário falhou.',
+
+'update_successful' =>
+'A atualziação do módulo Calendário foi um sucesso.',
+
+//----------------------------------------
+//  Menu Principal
+//----------------------------------------
+
+'calendars' =>
+'Calendários',
+
+'events' =>
+'Eventos',
+
+'occurrences' =>
+'Ocorrências',
+
+'reminders' =>
+'Lembretes',
+
+'permissions' =>
+'Permissões',
+
+'preferences' =>
+'Preferências',
+
+'documentation' =>
+'Documentação Online',
+
+'online_documentation' =>
+'Documentação Online',
+
+//----------------------------------------
+//  Publicar/Editar
+//----------------------------------------
+
+'filter' =>
+'Filtro',
+
+'remove_edited_occurrences' =>
+'Remover Ocorrências Editadas',
+
+'remove_edited_occurrences_desc' =>
+'Desmarque isto se você estiver editando detalhes sobre este evento. É recomendável você deixar isto marcado apenas se você estiver alterando eventos de EPOCAS, DATAS OU DATAS FINAIS, como não fazer irá criar eventos órfãos.',
+
+'create_calendar_first' =>
+'Você deve primeiro criar um calendário antes de criar um evento.',
+
+'select_a_calendar' =>
+'Selecionar um calendário',
+
+'new_date' =>
+'Nova Data',
+
+'add_rule' =>
+'Adicionar Regra',
+
+'editing_occurrence' =>
+'Editando a ocorrência da data <strong>%date%</strong> de <strong>%title%</strong>',
+
+'type' =>
+'Tipo',
+
+'include' =>
+'Incluir',
+
+'exclude' =>
+'Excluir',
+
+'repeat' =>
+'Repetir',
+
+'none' =>
+'Nenhum',
+
+'daily' =>
+'Diariamente',
+
+'weekly' =>
+'Semanalmente',
+
+'monthly' =>
+'Mensalmente',
+
+'yearly' =>
+'Anualmente',
+
+'select_dates' =>
+'Selecionar Datas',
+
+'all_day_event' =>
+'Evento Dia Todo',
+
+'from' =>
+'De',
+
+'to' =>
+'Para',
+
+'every' =>
+'A cada',
+
+'day_s' =>
+'dia(s)',
+
+'week_s_on' =>
+'semana(s) em',
+
+'at' =>
+'até',
+
+'month_s_by_day_of' =>
+'mês(es) por dia de',
+
+'1st' =>
+'1º',
+
+'2nd' =>
+'2º',
+
+'3rd' =>
+'3º',
+
+'4th' =>
+'4º',
+
+'5th' =>
+'5º',
+
+'only_on' =>
+'Apenas em',
+
+'year_s' =>
+'ano(s)',
+
+'end' =>
+'Fim',
+
+'never' =>
+'Nunca',
+
+'by_date' =>
+'por Data',
+
+'after' =>
+'Após',
+
+'time_s' =>
+'vez(es)',
+
+//----------------------------------------
+//  painel dos calendários
+//----------------------------------------
+
+'calendar_id' =>
+'ID do Calendário',
+
+'calendar_name' =>
+'Nome do Calendário',
+
+'status' =>
+'Status',
+
+'total_events' =>
+'Total de Eventos',
+
+//----------------------------------------
+//  Painel dos Eventos
+//----------------------------------------
+
+'event_id' =>
+'ID do Evento',
+
+'event_name' =>
+'Nome do Evento',
+
+'recurs' =>
+'Recorrente',
+
+'first_date' =>
+'Data Inicial',
+
+'last_date' =>
+'Data Final',
+
+'filter_events' =>
+"Filtrar Eventos",
+
+'filter_by_calendar' =>
+'Filtrar por Calendário',
+
+'filter_by_status' =>
+'Filtrar por Status',
+
+'filter_by_recurs' =>
+'Filtrar por Recorrência',
+
+'order_by' =>
+'Ordenar por',
+
+'date_is' =>
+'Data é',
+
+'event_id' =>
+'ID do Evento',
+
+'event_title' =>
+'Título do Evento',
+
+'calendar_name' =>
+'Nome do Calendário',
+
+'status' =>
+'Status',
+
+'recurs' =>
+'Recorrente',
+
+'first_date_is' =>
+'Data inicial é',
+
+'last_date' =>
+'Data final',
+
+'ascending' =>
+'Crescente',
+
+'descending' =>
+'Decrescente',
+
+'or_later' =>
+'Ou Depois',
+
+'or_earlier' =>
+'Ou Antes',
+
+'this_date' =>
+'Esta Data',
+
+'time' =>
+'Hora',
+
+'all_day' =>
+'Dia Todo',
+
+//----------------------------------------
+//  painel de exclusão de eventos
+//----------------------------------------
+
+'delete' =>
+'Excluir',
+
+'delete_events' =>
+'Excluir Eventos',
+
+'delete_events_title' =>
+'Excluir evento(s)?',
+
+'delete_events_question' =>
+'Realmente deseja EXCLUIR {COUNT} evento(s)?',
+
+'events_deleted' =>
+'Evento(s) foram excluídos',
+
+//----------------------------------------
+//  Ocorrências do painel
+//----------------------------------------
+
+'occurrence_id' =>
+'ID da Ocorrência',
+
+'event_date' =>
+'Data do Evento',
+
+'limit' =>
+'Limite',
+
+'page_limit' =>
+'Limite de Página',
+
+'occurrences_limit' =>
+'Limite de Ocorrências',
+
+// -------------------------------------
+//	Permissões
+// -------------------------------------
+
+'calendar_permissions_desc' =>
+"As Permissões podem ser definidas no momento de criação do calendário, ou alterada aqui. Super Admins e os grupos selecionados em 'Permitir Acesso Compelto para Grupos' que terão acesso a todos os calendários. Grupos selecionados em 'Bloquear Acesso para Grupos' não terão acesso aos calendários.",
+
+'allowed_groups' =>
+"Grupos Permitidos",
+
+'allow_full_access' =>
+'Permitir Acesso Completo para Grupos',
+
+'permissions_enabled' =>
+'Permissões Ativadas',
+
+'save_permissions' =>
+'Salvar Permissões',
+
+'all_groups' =>
+'Todos os Grupos',
+
+
+'allow_all' =>
+'Permitir tudo',
+
+'deny_all_access' =>
+'Bloquear Todo Acesso para Grupos',
+
+'deny_takes_precedence' =>
+"Tem precedência sobre o Permitir Tudo no Calendário",
+
+'permissions_saved' =>
+"Permissões Salvas",
+
+'group_permissions' =>
+'Permisssões do Grupo',
+
+'permissions_instructions' =>
+"Escolha os grupos que voc6e desejaria editar o acesso ao calendário e seus eventos. (Super Admins sempre tem acesso a todos os calendários. Se um grupo não aparece, pode ser que ele esteja com as permissões alteradas na aba de permissões no painel de controle do Calendário.",
+
+'disallowed_behavior_for_edit_page' =>
+"Comportamento não-permitido para a Página de Edição",
+
+'none' =>
+"Nenhum",
+
+'search_filter' =>
+"Filtro da Busca",
+
+'disable_link' =>
+"Desativar Link",
+
+'permission_dialog_title' =>
+"Erro de Permissão",
+
+'ok' => "OK",
+
+//----------------------------------------
+//  Preferencias do Painel
+//----------------------------------------
+
+'preference' =>
+'Preferência',
+
+'setting' =>
+'Configuração',
+
+'description' =>
+'Descrição',
+
+'first_day_of_week' =>
+'Primeiro Dia da Semana',
+
+'first_day_of_week_description' =>
+'Domingo e Segunda são geralmente as opções mais sensatas.',
+
+'clock_type' =>
+'Tipo de Hora',
+
+'clock_type_description' =>
+'Usar 12-horas ou 24-horas no relógio do painel de controle?',
+
+'12_hour' =>
+'12-hora',
+
+'24_hour' =>
+'24-horas',
+
+'default_timezone' =>
+'Fuso Horário Padrão',
+
+'default_timezone_description' =>
+'Fuso padRão para Novos Calendários.',
+
+'preferences_updated' =>
+'Prefereências atualizadas.',
+
+'default_date_format' =>
+'Formato da Data do Selecionador',
+
+'default_date_format_description' =>
+'Formato da Data do Selecionador de Data.',
+
+'default_time_format' =>
+'Formato Padrão de Hora',
+
+'default_time_format_description' =>
+'O padrão de horário padrão que será utilizado nos novos calendários.',
+
+'calendar_weblog' =>
+'Canal(is) do Calendário(s)',
+
+'calendar_weblog_description' =>
+'Canal(is) para designar como canais do(s) Calendário(s)',
+
+'event_weblog' =>
+'Canais dos Eventos',
+
+'event_weblog_description' =>
+'Canal(is) para designar como os receptores dos eventos',
+
+//----------------------------------------
+//  Botões
+//----------------------------------------
+
+'save' =>
+'Salvar',
+
+'delete_selected_items' =>
+'Exclur Itens Selecionados',
+
+//----------------------------------------
+//  Erros
+//----------------------------------------
+
+'no_results' =>
+'Sem resultados.',
+
+'no_title' =>
+'Nenhum título',
+
+'invalid_request' =>
+"Solicitação inválida",
+
+'calendar_module_disabled' =>
+"O módulo Calendário está desativado no momento. Por favor tenha certeza que ele esteja instalado e atualizado, indo no painel de conytrole do módulo no painel de módulos do ExpressionEngine",
+
+'disable_module_to_disable_extension' =>
+"Para desativar esta extensão, você deve desativar seu <a href='%url%'>módulo</a> correspondente.",
+
+'enable_module_to_enable_extension' =>
+"Para ativar esta extensão, você deve instalar seu <a href='%url%'>módule</a> correspondente.",
+
+'cp_jquery_requred' =>
+"A extensão 'jQuery para o Painel de Controle' deve estar <a href='%extensions_url%'>ativada</a> para utilizar este módulo.",
+
+'invalid_weblog_id' =>
+'ID de Canal Inválido',
+
+'invalid_entry_id' =>
+'ID de Registro Inválido',
+
+'invalid_site_id' =>
+'ID de Site Inválido',
+
+'invalid_calendar_id' =>
+'ID de Calendário Inválido',
+
+'invalid_ymd' =>
+'Data Inválida',
+
+'invalid_start_date' =>
+'Data Inicial Inválida',
+
+'invalid_end_date' =>
+'Data Final inválida',
+
+'invalid_year' =>
+'Ano Inválido',
+
+'invalid_month' =>
+'Mês Inválido',
+
+'invalid_day' =>
+'Dia Inválido',
+
+'invalid_date' =>
+'Data Inválida',
+
+'invalid_time' =>
+'Horário Inválido',
+
+'invalid_start_time' =>
+'Hora inicial inválida',
+
+'invalid_end_time' =>
+'Hora final inválida',
+
+'invalid_hour' =>
+'Hora Inválida',
+
+'invalid_minute' =>
+'Minuto inválido',
+
+'invalid_repeat_dates' =>
+'Datas de repetição inválidas',
+
+'invalid_calendar_permissions' =>
+'Você não possui permissão para editar ou adicionar eventos neste calendário',
+
+'no_permissions_for_any_calendars' =>
+"Você não possui permissão para adicionar ou editar eventos em qualquer calendário",
+
+'invalid_permissions_json_request' =>
+"Em uma solicitação válida JSON. Precisa de group_id e EE 2.x+.",
+
+//----------------------------------------
+//  Dias
+//----------------------------------------
+
+'day_1_full' =>
+'Segunda',
+
+'day_2_full' =>
+'Terça',
+
+'day_3_full' =>
+'Quarta',
+
+'day_4_full' =>
+'Quinta',
+
+'day_5_full' =>
+'Sexta',
+
+'day_6_full' =>
+'Sábado',
+
+'day_0_full' =>
+'Domingo',
+
+//----------------------------------------
+//  Dias - 2 Letras
+//----------------------------------------
+
+'day_1_2' =>
+'Se',
+
+'day_2_2' =>
+'Te',
+
+'day_3_2' =>
+'Qa',
+
+'day_4_2' =>
+'Qi',
+
+'day_5_2' =>
+'Se',
+
+'day_6_2' =>
+'Sa',
+
+'day_0_2' =>
+'Do',
+
+//----------------------------------------
+//  Days - 3 Letters
+//----------------------------------------
+
+'day_1_3' =>
+'Seg',
+
+'day_2_3' =>
+'Ter',
+
+'day_3_3' =>
+'Qua',
+
+'day_4_3' =>
+'Qui',
+
+'day_5_3' =>
+'Sex',
+
+'day_6_3' =>
+'Sáb',
+
+'day_0_3' =>
+'Dom',
+
+//----------------------------------------
+//  Dias - Curtos
+//----------------------------------------
+
+'day_1_short' =>
+'Seg',
+
+'day_2_short' =>
+'Ter',
+
+'day_3_short' =>
+'Qua',
+
+'day_4_short' =>
+'Qui',
+
+'day_5_short' =>
+'Sex',
+
+'day_6_short' =>
+'Sáb',
+
+'day_0_short' =>
+'Dom',
+
+//----------------------------------------
+//  Dias - 1 letra
+//----------------------------------------
+
+'day_1_1' =>
+'S',
+
+'day_2_1' =>
+'T',
+
+'day_3_1' =>
+'Q',
+
+'day_4_1' =>
+'Q',
+
+'day_5_1' =>
+'S',
+
+'day_6_1' =>
+'S',
+
+'day_0_1' =>
+'D',
+
+//----------------------------------------
+//  Ordinal suffixes
+//----------------------------------------
+
+'suffix_0' =>
+'º',
+
+'suffix_1' =>
+'º',
+
+'suffix_2' =>
+'º',
+
+'suffix_3' =>
+'º',
+
+'suffix_4' =>
+'º',
+
+'suffix_5' =>
+'º',
+
+'suffix_6' =>
+'º',
+
+'suffix_7' =>
+'º',
+
+'suffix_8' =>
+'º',
+
+'suffix_9' =>
+'º',
+
+'suffix_10' =>
+'º',
+
+'suffix_11' =>
+'º',
+
+'suffix_12' =>
+'º',
+
+'suffix_13' =>
+'º',
+
+'suffix_14' =>
+'º',
+
+'suffix_15' =>
+'º',
+
+'suffix_16' =>
+'º',
+
+'suffix_17' =>
+'º',
+
+'suffix_18' =>
+'º',
+
+'suffix_19' =>
+'º',
+
+//----------------------------------------
+//  Meses
+//----------------------------------------
+
+'month_1_full' =>
+'Janeiro',
+
+'month_2_full' =>
+'Fevereiro',
+
+'month_3_full' =>
+'Março',
+
+'month_4_full' =>
+'Abril',
+
+'month_5_full' =>
+'Maio',
+
+'month_6_full' =>
+'Junho',
+
+'month_7_full' =>
+'Jullho',
+
+'month_8_full' =>
+'Agosto',
+
+'month_9_full' =>
+'Setembro',
+
+'month_10_full' =>
+'Outubro',
+
+'month_11_full' =>
+'Novembro',
+
+'month_12_full' =>
+'Dezembro',
+
+//----------------------------------------
+//  Meses - 3 letras
+//----------------------------------------
+
+'month_1_3' =>
+'Jan',
+
+'month_2_3' =>
+'Fev',
+
+'month_3_3' =>
+'Mar',
+
+'month_4_3' =>
+'Abr',
+
+'month_5_3' =>
+'Mai',
+
+'month_6_3' =>
+'Jun',
+
+'month_7_3' =>
+'Jul',
+
+'month_8_3' =>
+'Ago',
+
+'month_9_3' =>
+'Set',
+
+'month_10_3' =>
+'Out',
+
+'month_11_3' =>
+'Nov',
+
+'month_12_3' =>
+'Dez',
+
+//----------------------------------------
+//  am/pm
+//----------------------------------------
+
+'am' =>
+'am',
+
+'pm' =>
+'pm',
+
+'AM' =>
+'AM',
+
+'PM' =>
+'PM',
+
+'am_dot' =>
+'a.m.',
+
+'pm_dot' =>
+'p.m.',
+
+//----------------------------------------
+//  Parametros da data
+//----------------------------------------
+
+'today' =>
+'hoje',
+
+'yesterday' =>
+'ontem',
+
+'tomorrow' =>
+'amanhã',
+
+'day' =>
+'dia',
+
+'week' =>
+'semana',
+
+'month' =>
+'mês',
+
+'year' =>
+'ano',
+
+'ago' =>
+'atrás',
+
+'begin' =>
+'início',
+
+'last' =>
+'final',
+
+//----------------------------------------
+//  Parametros de horário
+//----------------------------------------
+
+'now' =>
+'agora',
+
+'noon' =>
+'meio-dia',
+
+'midnight' =>
+'meia-noite',
+
+//----------------------------------------
+//  descritivos do campo
+//----------------------------------------
+
+'summary' =>
+'Descritivo',
+
+'location' =>
+'Local',
+
+'dates_and_options' =>
+'Datas & Opções',
+
+'ics_url_label' =>
+'URL para o iCalendar arquivo (.ics)',
+
+'ics_url_desc' =>
+"Adicione uma ou mais URLS para aqruivos .ics - separados por novas linhas - para importar este calendário. Todos os horários importados serão ajustados para estas configurações de fuso horário.",
+
+'ics_url_stub' =>
+"Todos os horários importados serão ajustados para estas configurações de fuso horário do calendário.",
+
+'time_format_label' =>
+'Formato do Horário',
+
+'time_format_desc' =>
+'Formato padrão d etempo para usar neste calendário.',
+
+'timezone' =>
+'Fuso Horário',
+
+/* FIM */
+''=>''
+);
